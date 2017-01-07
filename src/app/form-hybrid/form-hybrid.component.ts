a7c5687e7abc9d16b12b550d0b3f52b3ddf5cb22
Christian Crowhurst <ccrowhurst@ramplc.com>
2017-01-07 18:14:57 +0000

    more form examples

--- src/app/form-hybrid/form-hybrid.component.ts
+++ src/app/form-hybrid/form-hybrid.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { FormGroup, NgForm } from '@angular/forms'
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { FormGroup, NgForm, NgModel } from '@angular/forms'
 import { Observable } from 'rxjs'
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
 
 /**
  * Example of combining template-driven forms with reactive code ie using
- * the valueChanges EventEmitter (an rxjs Subject) to allow code to reactive
+ * the valueChanges EventEmitter (an rxjs Subject) to allow code to react
  * to the form changes over time
  */
 
@@ -15,17 +16,47 @@ import 'rxjs/add/operator/map';
   templateUrl: './form-hybrid.component.html',
   styleUrls: ['./form-hybrid.component.css']
 })
-export class FormHybridComponent implements OnInit, AfterViewInit {
+export class FormHybridComponent implements OnInit, AfterViewChecked {
   @ViewChild('myForm') currentForm: NgForm;
-  formValue$: Observable<any>;
+  @ViewChild('age') ageElem: ElementRef;
+  @ViewChild('salaryModel') salaryModel: NgModel;
+  salary = 20000;
+  formValue$: Observable<string>;
+  salaryValue$: Observable<number>;
 
   constructor() { }
 
   ngOnInit() {
+    this.salaryValue$ = this.salaryModel.valueChanges
+      .do(v => {
+        console.log(`salaryValue$: ${v}`)
+      });
+
+    this.formValue$ = this.currentForm.valueChanges
+      .map(stringify)
+      .map(json => `formValue$: ${json}`)
+      .do(console.log);
+
+    this.salaryValue$.subscribe(v => {
+      console.log(`ngOnInit salaryValue$ subs: ${v}`);
+    });
   }
-  
-  ngAfterViewInit(){
-    this.formValue$ = this.currentForm.valueChanges.map(stringify).do(console.log);
+
+  ngAfterViewChecked() {
+    if (this.ageInput.value) {
+      console.log(`ngAfterViewChecked -> ageInput.value: ${this.ageInput.value}`);
+    }
+    if (this.salaryModel.value) {
+      console.log(`ngAfterViewChecked -> salaryModel.value: ${this.salaryModel.value}`);
+    }
+  }
+
+  setSalary(val: number) {
+    this.salary = val;
+  }
+
+  get ageInput() {
+    return this.ageElem.nativeElement as HTMLInputElement;
   }
 }
 
