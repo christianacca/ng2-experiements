190edfba9a07db5d824493522e3cc44153911e22
Christian Crowhurst <ccrowhurst@ramplc.com>
2017-05-01 15:32:00 +0100

    partially fix for https://github.com/angular/angular/issues/13614
    This fixes only non-AOT builds

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,21 +1,36 @@
 import { ASSET_TYPE_PROVIDER, Asset, AssetType } from './asset';
 import { COMPANY_TYPE_PROVIDER, Company, CompanyType } from './company';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { Bootstrappable } from './bootstrappable.service';
 import { Bootstrappable2 } from './bootstrappable2.service';
 import { Db } from './db.service';
 import { DelegatingErrorHandler } from './delegating-error-handler.service';
-import { provideBootstrappable } from './provide-bootstrappable';
+import { provideBootstrappable, runBlockFactory } from './provide-bootstrappable';
 
 export { Company, CompanyType };
 export { Asset, AssetType };
 export { Bootstrappable, Bootstrappable2 };
 
 // making an exported variable to try and solve issue: https://github.com/angular/angular/issues/13614 
-export let bootstrappableProvider = provideBootstrappable(Bootstrappable);
-export let bootstrappableProvider2 = provideBootstrappable(Bootstrappable2);
-export let errorProvider = { provide: ErrorHandler, useClass: DelegatingErrorHandler };
+// todo: replace provider with commented out lines once issue 13614 is resolved
+export const bootstrappableProvider = [{
+        provide: APP_INITIALIZER,
+        multi: true,
+        useFactory: runBlockFactory,
+        deps: [Bootstrappable]
+    }, Bootstrappable];
+export const bootstrappableProvider2 = [{
+        provide: APP_INITIALIZER,
+        multi: true,
+        useFactory: runBlockFactory,
+        deps: [Bootstrappable2]
+    }, Bootstrappable2];
+/*
+export const bootstrappableProvider = provideBootstrappable(Bootstrappable);
+export const bootstrappableProvider2 = provideBootstrappable(Bootstrappable2);
+*/
+export const errorProvider = { provide: ErrorHandler, useClass: DelegatingErrorHandler };
 
 @NgModule({
     providers: [
