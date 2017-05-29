a7c0615065133189b8c85fcda2f5679e6846ba1f
christianacca <christian.crowhurst@gmail.com>
2017-05-29 14:50:00 +0100

    save point

--- src/app/main/change-detection/detatched-obs/counter.component.ts
+++ src/app/main/change-detection/detatched-obs/counter.component.ts
@@ -1,28 +1,41 @@
-import { Component, Input, ChangeDetectionStrategy, Optional, OnInit } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, Optional, SimpleChanges, SimpleChange, OnChanges } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { AttachedIfDirective } from '../../../shared/attached-if.directive';
 
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/withLatestFrom';
+
+interface Inputs extends SimpleChanges {
+  value: SimpleChange;
+  pauseWhenDetatched: SimpleChange;
+}
 
 @Component({
   selector: 'app-counter',
   template: `
-    {{_value$ | async}}
+    {{currentValue | async}}
   `,
   styles: [`
     :host { display: block }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent implements OnInit {
-  @Input() value: Observable<number>;
+export class CounterComponent implements OnChanges {
+
+  currentValue: Observable<number>;
+
+  // tslint:disable-next-line:no-input-rename
+  @Input('value') rawValue: Observable<number>;
+
   @Input() pauseWhenDetatched = false;
 
-  public _value$: Observable<number>;
+  sourceValue: Observable<number>;
 
   private get isAttached() {
     if (this.attachedIf == null) { return true; }
@@ -30,19 +43,22 @@ export class CounterComponent implements OnInit {
     return this.attachedIf.isAttached;
   }
 
-  private onAttach$: Observable<boolean>;
-
   constructor( @Optional() private attachedIf: AttachedIfDirective) {
-    this.onAttach$ = attachedIf ? attachedIf.onAttach : new BehaviorSubject(true);
   }
 
-  ngOnInit(): void {
-    // note: even when this component change detector is detatached the component will still
+  ngOnChanges(changes: SimpleChanges): void {
     // recieve calls to emissions from `this.value`
     // we use `pauseWhenDetatched` to determine whether values emitted by `this.value` will be "dropped"
     // (ie not emitted onto `this._value$`)
-    this._value$ = this.value
-      .switchMap(v => this.isAttached || !this.pauseWhenDetatched ? Observable.of(v) : Observable.empty())
-      .do(count => console.log(`counter next: ${count}`));
+
+    if (this.pauseWhenDetatched && this.attachedIf) {
+      const value = this.rawValue.share();
+      const attachedValue = value.switchMap(v => this.isAttached ? Observable.of(v) : Observable.empty())
+        .do(count => console.log(`counter next: ${count}`));
+      const reattachValue = this.attachedIf.onAttach.withLatestFrom(value, (_, v) => v);
+      this.currentValue = Observable.merge(attachedValue, reattachValue);
+    } else {
+      this.currentValue = this.rawValue.do(count => console.log(`counter next: ${count}`));
+    }
   }
 }
