921290c04ede6c9d33acb8265cba7dc8ef97ad95
christianacca <christian.crowhurst@gmail.com>
2017-06-19 15:41:44 +0100

    make AOT happy

--- src/app/core/int.decorator.ts
+++ src/app/core/int.decorator.ts
@@ -1,9 +1,15 @@
-import { convert } from './convert.decorator';
+import { addPropertyTrait, PropertyTrait } from './add-property-trait';
 
-function parseValue(value: any) {
+
+export function parseValue(value: any) {
     if (value == null || typeof value === 'number') { return value; }
 
     return parseInt(value, 10);
 }
 
-export const int = convert(parseValue);
+const propertyTrait: PropertyTrait = {
+    parser: parseValue
+};
+export function int(target: any, propertyKey: string) {
+    addPropertyTrait(target, propertyKey, propertyTrait);
+}
