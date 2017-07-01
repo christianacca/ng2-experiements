3f067075bf2e3e5d0f5d373b8c971b62551e3786
christianacca <christian.crowhurst@gmail.com>
2017-07-01 13:56:56 +0100

    cd

--- src/app/main/change-detection/differ/key-value-differ-form.component.ts
+++ src/app/main/change-detection/differ/key-value-differ-form.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input, KeyValueChanges, KeyValueDiffer, KeyValueDiffers, DoCheck } from '@angular/core';
 import { Model, ModelClass } from './model';
 import { NgForm } from '@angular/forms';
-import { KeyValueDifferSubject } from './key-value-differ-observable';
+import { KeyValueDifferSubject, ChangeCollection } from './key-value-differ-observable';
 import './key-value-differ-operator';
 import { Observable } from 'rxjs/Observable';
 import { AutoUnsubscribe } from '../../../core';
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/reduce';
 
 @Component({
   selector: 'app-key-value-differ-form',
@@ -14,30 +16,58 @@ import { Subscription } from 'rxjs/Subscription';
 })
 @AutoUnsubscribe
 export class KeyValueDifferFormComponent implements DoCheck, OnInit {
-  logsSubscription: Subscription;
   @Input() model: Model;
-
-  modelSubject: KeyValueDifferSubject<any>;
-  private counter = 0;
+  modelDiffs: KeyValueDifferSubject<Model>;
   logs$: Observable<string[]>;
-  constructor(private differs: KeyValueDiffers) {}
+  addCount$: Observable<number>;
+  changeCount$: Observable<number>;
+  removeCount$: Observable<number>;
+  subscriptions = new Subscription();
+  constructor(private differs: KeyValueDiffers) { }
 
   ngOnInit() {
-    const modelDiffer = this.differs.find(this.model).create();
-    this.modelSubject = Observable.diff(this.model, modelDiffer);
+    this.modelDiffs = Observable.diff(this.model, this.differs);
 
-    // we need to use a replayable observable to avoid the async pipe in our template missing the first
-    // set of changes
-    const logs$ = this.modelSubject
-      .map(changes => this.formatChanges(changes))
-      .scan((acc, current) => current.concat(acc), [])
+    // we need to `publishReplay` to avoid the async pipe in our template
+    // missing the first set of changes
+
+    const birthdaysOnly = (changes: KeyValueChanges<keyof Model, any>) => {
+      return Observable.ofChanges<Model>(changes)
+        .includes(r => r.key === 'age')
+        .mapToRecordsOf('change')
+        .filter(r => r.key === 'age');
+    }
+
+    const logs$ = this.modelDiffs
+      .switchMap(changes =>
+        birthdaysOnly(changes)
+          .map(r => [`Happy ${r.currentValue}`])
+          .merge([this.toLogEntries(changes)])
+          .reduce((prev, current) => current.concat(prev))
+      )
       .publishReplay(1);
-    this.logsSubscription = logs$.connect();
+    this.subscriptions.add(logs$.connect());
     this.logs$ = logs$;
+
+    this.addCount$ = this.countOf$('add');
+    this.removeCount$ = this.countOf$('remove');
+    this.changeCount$ = this.countOf$('change');
+
   }
 
+  private countOf$(collection: ChangeCollection): Observable<number> {
+    const o = this.modelDiffs
+      .mapToArrayOf(collection)
+      .scan((sum, records) => sum + records.length, 0)
+      .publishReplay(1)
+    this.subscriptions.add(o.connect());
+    return o;
+  }
+
+
+
   ngDoCheck(): void {
-    this.modelSubject.detectChanges();
+    this.modelDiffs.detectChanges();
   }
 
   apply(form: NgForm, target: object) {
@@ -52,18 +82,8 @@ export class KeyValueDifferFormComponent implements DoCheck, OnInit {
     delete this.model.child;
   }
 
-  formatChanges(changes: KeyValueChanges<string, any>) {
+  toLogEntries(changes: KeyValueChanges<string, any>) {
     const logs: string[] = [];
-    if (changes == null) {
-      const msg = `no change`;
-      if (logs.length && logs[0].startsWith(msg)) {
-        logs[0] = `no change (${this.counter++})`;
-      } else {
-        this.counter = 0;
-        logs.unshift(`no change`);
-      }
-      return;
-    }
     changes.forEachChangedItem(r => {
       logs.unshift(`${r.key} - change: ${JSON.stringify(r.currentValue)} (prev: ${JSON.stringify(r.previousValue)})`)
     });
--- src/app/main/change-detection/differ/key-value-differ-observable.ts
+++ src/app/main/change-detection/differ/key-value-differ-observable.ts
@@ -2,23 +2,34 @@ import { KeyValueDiffer, KeyValueChangeRecord, KeyValueChanges } from '@angular/
 import { Observable } from 'rxjs/Observable';
 import { Subscriber } from 'rxjs/Subscriber';
 import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/switchMap';
 
+export type ChangeCollection = 'add' | 'remove' | 'change';
 
-function toChangeRecords<K, V>(changes: KeyValueChanges<K, V>) {
+function toArray<K, V>(changes: KeyValueChanges<K, V>, collections?: ChangeCollection[]) {
     const result: KeyValueChangeRecord<K, V>[] = [];
-    changes.forEachChangedItem(r => {
-        result.push(r);
-    });
-    changes.forEachAddedItem(r => {
-        result.push(r);
-    });
-    changes.forEachRemovedItem(r => {
-        result.push(r);
-    });
+    if (collections === undefined || collections.includes('change')) {
+        changes.forEachChangedItem(r => {
+            result.push(r);
+        });
+    }
+    if (collections === undefined || collections.includes('add')) {
+        changes.forEachAddedItem(r => {
+            result.push(r);
+        });
+    }
+    if (collections === undefined || collections.includes('remove')) {
+        changes.forEachRemovedItem(r => {
+            result.push(r);
+        });
+    }
     return result;
 }
 
-function hasChange<K, V>(changes: KeyValueChanges<K, V>, predicate: (record: KeyValueChangeRecord<K, V>) => boolean) {
+function hasRecord<K, V>(changes: KeyValueChanges<K, V>, predicate: (record: KeyValueChangeRecord<K, V>) => boolean) {
     let match = false;
     const shortCiruitingPredicate = (r: KeyValueChangeRecord<K, V>) => {
         return match = match || predicate(r);
@@ -31,24 +42,37 @@ function hasChange<K, V>(changes: KeyValueChanges<K, V>, predicate: (record: Key
     if (match) { return true; }
 }
 
-export class KeyValueDifferObservable extends Observable<KeyValueChanges<string, any>> {
-    filterByRecord(predicate: (record: KeyValueChangeRecord<string, any>) => boolean) {
-        return new KeyValueDifferObservable(subscriber => {
-            return this.filter(changes => hasChange(changes, predicate)).subscribe(subscriber);
+export class KeyValueDifferObservable<T extends object, K extends keyof T = keyof T> extends Observable<KeyValueChanges<K, any>> {
+    mapToArrayOf(...collections: ChangeCollection[]) {
+        return this.map(changes => toArray(changes, collections));
+    }
+    includes(predicate: (record: KeyValueChangeRecord<K, any>) => boolean) {
+        return new KeyValueDifferObservable<T, K>(subscriber => {
+            return this.filter(changes => hasRecord(changes, predicate)).subscribe(subscriber);
         })
     }
+    mapToArray() {
+        return this.map(changes => toArray(changes));
+    }
     mapToRecords() {
-        return this.map(toChangeRecords);
+        return this
+            .map(changes => toArray(changes))
+            .switchMap(records => Observable.from(records));
+    }
+    mapToRecordsOf(...collections: ChangeCollection[]) {
+        return this
+            .map(changes => toArray(changes, collections))
+            .switchMap(records => Observable.from(records));
     }
 }
 
 
 
-export class KeyValueDifferSubject<T extends object> extends KeyValueDifferObservable {
-    private subject = new Subject<KeyValueChanges<string, any>>();
+export class KeyValueDifferSubject<T extends {}, K extends keyof T = keyof T> extends KeyValueDifferObservable<T, K> {
+    private subject = new Subject<KeyValueChanges<K, any>>();
 
-    static create<T extends object>(source: T, differ: KeyValueDiffer<string, any>) {
-        return new KeyValueDifferSubject<T>(source, differ);
+    static create<T extends object, K extends keyof T>(source: T, differ: KeyValueDiffer<K, any>) {
+        return new KeyValueDifferSubject<T, K>(source, differ);
     }
 
     detectChanges() {
@@ -60,8 +84,8 @@ export class KeyValueDifferSubject<T extends object> extends KeyValueDifferObser
 
     private constructor(
         public value: T,
-        private differ: KeyValueDiffer<string, any>) {
-        super((subscriber: Subscriber<KeyValueChanges<string, any>>) => {
+        private differ: KeyValueDiffer<K, any>) {
+        super((subscriber: Subscriber<KeyValueChanges<K, any>>) => {
             return this.subject.subscribe(subscriber);
         });
     }
