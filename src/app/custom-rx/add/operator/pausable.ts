c012e0c0adfc65ba681f8bec4e33b5b6f8533df3
christianacca <christian.crowhurst@gmail.com>
2017-07-09 22:57:44 +0100

    remove operator type duplication

--- src/app/custom-rx/add/operator/pausable.ts
+++ src/app/custom-rx/add/operator/pausable.ts
@@ -3,7 +3,7 @@ import { pauseReplay } from '../../pausable.operator'
 
 declare module 'rxjs/Observable' {
   interface Observable<T> {
-    pauseReplay(pauser: Observable<any>): Observable<T>;
+    pauseReplay: typeof pauseReplay;
   }
 }
 
