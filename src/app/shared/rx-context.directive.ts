75c40ae13e93c3e579d6a75d58e672d6c27e3aa3
christianacca <christian.crowhurst@gmail.com>
2017-07-20 20:53:44 +0100

    fix RxContext - ensure correct disposal of existing observable

--- src/app/shared/rx-context.directive.ts
+++ src/app/shared/rx-context.directive.ts
@@ -6,25 +6,36 @@ import { Subscription } from 'rxjs/Subscription';
     // tslint:disable-next-line:directive-selector
     selector: '[rxContext][rxContextOn]'
 })
-export class RxContextDirective implements OnInit, OnDestroy {
-    subs: Subscription;
+export class RxContextDirective implements OnDestroy {
+    private subs: Subscription;
     private viewRef: EmbeddedViewRef<any>;
-    @Input() rxContextOn: Observable<any>;
+    @Input()
+    set rxContextOn(value: Observable<any>) {
+        this.disposeViewSubscription();
+        this.createViewSubscription(value);
+    }
+
     constructor(private template: TemplateRef<any>, private viewContainer: ViewContainerRef) { }
 
     ngOnDestroy(): void {
+        this.disposeViewSubscription();
+    }
+
+    private createViewSubscription(state$: Observable<any>) {
+        this.subs = state$.subscribe(state => this.createOrUpdateView(state));
+    }
+
+    private createOrUpdateView(state: any) {
+        if (!this.viewRef) {
+            this.viewRef = this.viewContainer.createEmbeddedView(this.template, { '$implicit': state });
+        } else {
+            this.viewRef.context.$implicit = state;
+        }
+    }
+
+    private disposeViewSubscription() {
         if (!this.subs) { return; }
 
         this.subs.unsubscribe();
-    }
-
-    ngOnInit(): void {
-        this.subs = this.rxContextOn.subscribe(state => {
-            if (!this.viewRef) {
-                this.viewRef = this.viewContainer.createEmbeddedView(this.template, { '$implicit': state });
-            } else {
-                this.viewRef.context.$implicit = state;
-            }
-        });
     }
 }
