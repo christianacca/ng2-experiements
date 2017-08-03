f12e737b7c6fa8a253ef31924d01b35d9b451fba
christianacca <christian.crowhurst@gmail.com>
2017-08-03 07:41:26 +0100

    fixes to bootstrapping

--- src/app/bootstrapping/bootstrapping.module.ts
+++ src/app/bootstrapping/bootstrapping.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Provider, ModuleWithProviders, ErrorHandler } from '@angular/core';
+import { NgModule, Provider, ModuleWithProviders } from '@angular/core';
 import { bootstrapperProviders, Boostrapper } from './bootstrapper';
 import { LazyModuleBootstrapper } from './lazy-module-bootstrapper';
 
@@ -25,21 +25,10 @@ export class BootstrappingModule {
             ]
         };
     }
-    constructor(bootstrapper: LazyModuleBootstrapper, errorHandler: ErrorHandler) {
+    constructor(bootstrapper: LazyModuleBootstrapper) {
         // kick off bootstrapping for a lazy loaded module as soon as it's loaded
         // that way when the route is navigated to, any blocking bootstrapping
         // work will have already completed
-
-        // Q: why are we manually publishing via `handleError`, promise rejected errors
-        //    returned by `bootstrapper.run`?
-        //    Given that angular ordinarily does this for us when it sees that an error from a
-        //    promise is going unhandled (ie no catch callback registered with the promise)
-        // A: on testing when our `BootstrappingModule.ctor` is being invoked during lazy loading
-        //    of a route then angular will NOT pass the error that is about to be swallowed through
-        //    to ErrorHandler. Therefore we need to always do this ourselves even though this
-        //    would sometimes be redundant.
-        bootstrapper.run().catch(error => {
-            errorHandler.handleError(error);
-        });
+        bootstrapper.run();
     }
 }
--- src/app/bootstrapping/lazy-module-bootstrapper.ts
+++ src/app/bootstrapping/lazy-module-bootstrapper.ts
@@ -6,27 +6,16 @@ import { Boostrapper, BOOTSTRAPPABLE, createConfigAndRunBlock } from './bootstra
 @Injectable()
 export class LazyModuleBootstrapper implements CanActivate {
 
-    private done: Promise<boolean>;
+    private blockingDone: Promise<boolean>;
 
     constructor(
         @Inject(BOOTSTRAPPABLE) @Optional() private bootstrappable: Startable[],
-        @SkipSelf() @Optional() private bootstrapper: Boostrapper
+        @SkipSelf() @Optional() private bootstrapper: Boostrapper,
+        private errorHandler: ErrorHandler
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-        // Allow the first call to `run` to propogate any errors so that
-        // angular will log the problem.
-        // Thereafter, just deny the navigation by returning false
-        // In this way we avoid the same cached error being notified multiple
-        // times by the ErrorHandler service.
-        // Of course, it does assume that the dev will now disable the
-        // navigation thus giving the user a visual cue that the app is in a
-        // faulty state
-
-        if (this.done) {
-            return this.done.catch(() => false);
-        }
-        return this.run();
+        return this.blockingDone || this.run();
     }
 
     run() {
@@ -35,8 +24,31 @@ export class LazyModuleBootstrapper implements CanActivate {
             return Promise.resolve(true);
         }
 
-        this.done = createConfigAndRunBlock(this.bootstrappable, this.bootstrapper)()
+        // Q: why are we manually sending errors to `ErrorHandler.handleError`?
+        //    Given that angular would do that for us so long as we let the promise
+        //    rejection go "unhandled" (ie when no catch callback is registered with
+        //    the promise)
+        // A: angular would indeed send the error to `ErrorHandler.handleError` for us
+        //    However, it would keep sending the same promise rejection error every
+        //    time the user attempted to navigate.
+        //    Whereas we only want the error to be sent once and thereafter, just
+        //    deny the navigation by returning false
+
+        // Of course, returning false, whilst stopping navigation, it does assume that
+        // the dev will also disable the navigation action thus giving the user a visual
+        // cue that the app is in a faulty state
+
+        // note: not all boostrapping work will be blocking
+        // it's the dev's responsibility to prevent the menu items for a lazy
+        // loaded module that is faulting from being activated, not this service
+        // all this service is responsible for is for preventing the route from
+        // activating in case a blocking bootstrap fails
+        this.blockingDone = createConfigAndRunBlock(this.bootstrappable, this.bootstrapper)()
             .then(() => true)
-        return this.done;
+            .catch(error => {
+                this.errorHandler.handleError(error);
+                return false;
+            });
+        return this.blockingDone;
     }
 }
